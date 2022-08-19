ceabed54e43d8da371fa90a528edc19ed16375d5
asharudheenkp <asharudheenkpi786@gmail.com>
2022-08-19 21:43:08 +0530

    frontendOnly

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+const menuBtn = document.querySelector('.handbugermenu')
+const menu  = document.querySelector('.container')
+
+let slide = document.querySelector('fcard')
+
+
+// console.log(slide.length)
+
+
+
+
+menuBtn.addEventListener('click' , ()=>{
+    menu.classList.toggle('active')
+
+})
